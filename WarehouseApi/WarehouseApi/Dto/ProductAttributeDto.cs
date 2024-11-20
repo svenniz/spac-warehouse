8785fb67543ffb82cc70aec6d8cfd62180a063df
Andreas Højgaard-Gammeltoft <afhoejgaard@gmail.com>
2024-11-20 12:13:54 +0100

    Make ProductAttributeDto properties nullable

--- WarehouseApi/WarehouseApi/Dto/ProductAttributeDto.cs
+++ WarehouseApi/WarehouseApi/Dto/ProductAttributeDto.cs
@@ -4,6 +4,6 @@ namespace WarehouseApi.Dto;
 
 public class ProductAttributeDto
 {
-    public string Key { get; set; }
-    public string Value { get; set; }
+    public string? Key { get; set; }
+    public string? Value { get; set; }
 }
